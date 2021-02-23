f945b646286dbe578a1d90abbfb03f2a18293dad
Bruno Mariani <bruno0907@gmail.com>
2021-02-23 11:46:50 -0300

    Fixed _document.tsx file and added proper render methods

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import Head from 'next/head'
-
 import GlobalStyles from '../styles/GlobalStyles'
 
 function MyApp({ Component, pageProps }) {
   return (
   <>    
-    <GlobalStyles />
-    <Head>
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-      <title>Bem vindo ao Move.it</title>
-    </Head>
+    <GlobalStyles />    
     <Component {...pageProps} />
   </>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,24 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+  render(){
+    return(
+      <Html>
+        <Head>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+          <title>Bem vindo ao Move.it</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+  
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
